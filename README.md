cbef83eb1db08553a906e7e19980e8e5194645f4
lgentsch <lars.gentsch@epost-dev.de>
2014-10-18 12:15:58 +0200

    Added start Jetty 9.X during component test phase. Removed application plugin and added jetty-eclipse plugin.

--- README.md
+++ README.md
@@ -3,4 +3,10 @@ groovy-gradle-spring-boot
 
 Simple project to demonstrate Spring-Boot with Groovy and Gradle.
 
+Spring-Boot run as WAR:
 
+http://spring.io/guides/gs/convert-jar-to-war/#initial
+    
+The Gradle jetty-plugin is outdated -> uses Jetty 6.1 but at least Jetty 7 is needed for Servlet-API 3.0 an XML-less web app configuration!
+
+So this project uses the gradle-jetty-eclipse-plugin https://github.com/Khoulaiz/gradle-jetty-eclipse-plugin instead. 
--- src/test/groovy/example/HelloControlllerCT.groovy
+++ src/test/groovy/example/HelloControlllerCT.groovy
@@ -4,7 +4,13 @@ import spock.lang.Specification
 
 class HelloControlllerCT extends Specification {
 
-	def "GET on Hello resource returns a 200OK and a non-empty body"() {
-		expect: println "Hello, I am a ComponentTest!" 
-	}
+    def "GET on Hello resource returns a 200OK and a non-empty body"() {
+        when: def data = new URL('http://localhost:8080/').getText()
+        then: data
+    }
+
+    def "GET on Info resource returns a 200OK and a non-empty body"() {
+        when: def data = new URL('http://localhost:8080/info').getText()
+        then: data
+    }
 }
